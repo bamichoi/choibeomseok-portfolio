4d72cc539c8638a12a5401345715fdd1db2ffdef
Beomseok Choi <cbs412@naver.com>
2024-12-19 13:23:53 +0900

    task 문장 수정

--- src/e_entities/career/model/careerProjects.ts
+++ src/e_entities/career/model/careerProjects.ts
@@ -3,39 +3,38 @@ const careerProjects = [
     id: 1,
     summary: {
       projectTitle: "HyperChatbot 2.0",
-      description: "Rule-base 기반 챗봇 스튜디오 서비스",
+      description: "챗봇 스튜디오 FE 개발",
     },
     tasks: [
-      "- Chatflow가 입력된 챗봇의 빌드 및 배포 페이지 개발",
-      "- 빌드 및 배포 버전 관리를 위한 리스트 테이블 컴포넌트 개발",
-      "- SWR을 활용한 챗봇 버전 데이터 관리 및 Pagination 개발",
-      "- 커스터마이징이 용이한 반응형 드롭다운 메뉴 공통 컴포넌트 개발",
-      "- End-user 채팅 애플리케이션 개발",
+      "- 챗봇 빌드 및 배포와 버전 관리 페이지 개발",
+      "- 빌드 버전 및 배포 내역을 확인할 수 있는 테이블 리스트 컴포넌트 개발",
+      "- SWR을 활용한 Pagination 개발",
+      "- 확장성을 고려한 들보다운 메뉴 공통 컴포넌트 개발",
+      "- End-user 채팅 애플레키이션 STT 기능 및 UI 개발",
     ],
   },
   {
     id: 2,
     summary: {
       projectTitle: "A-Job",
-      description: "AI 기반 채용 지원 개인화 서비스",
+      description: "채용지원 개인화 서비스 FE 개발",
     },
     tasks: [
-      "- 다양한 형태의 기업 공고 리스트 및 개인 프로필 페이지 개발",
-      "- Swiper를 활용한 여러 유형의 슬라이드 UI 및 공통 컴포넌트 개발",
-      "- Portal을 이용한 모달 컴포넌트 개발",
+      "- Swiper를 활용한 다양한 형태의 채용 공고 슬라이드 UI 개발",
+      "- React Portal을 이용한 모달 컴포넌트 개발",
     ],
   },
   {
     id: 3,
     summary: {
       projectTitle: "TmaxAI Homepage",
-      description: "티맥스에이아이사 홈페이지 재구축 및 개선 프로젝트",
+      description: "자사 공식 홈페이지 FE 개발",
     },
     tasks: [
-      "- Parallex Scrolling Effect을 배경에 적용한 페이지 레이아웃 개발",
-      "- TTS 기술 체험 페이지 및 Audio Recorder 컴포넌트 개발",
-      "- 오디오 파일 전송 기반의 오프라인 STT 개발",
-      "- Websocket 및 AudioWorklet을 이용한 실시간 STT 기능 개발",
+      "- 페이지 배경에 Parallex Scrolling Effect를 적용한 공통 레이아웃 개발",
+      "- WebSocket과 AudioWorklet을 이용한 실시간 Audio 데이터 처리 및 STT 기술체험 기능 구현",
+      "- STT를 위한 Audio Recorder 및 AudioP layer 컴포넌트 개발",
+      "- TTS 기술 체험 UI 및 기능 개발",
     ],
   },
   {
@@ -45,9 +44,9 @@ const careerProjects = [
       description: "정부 디지털 교과서 개발사업",
     },
     tasks: [
-      "- STT/TTS 기능이 연동된 학습 및 평가용 녹음기 컴포넌트 개발",
-      "- ApexChart를 이용한 AI 발음 평가 결과 데이터 시각화",
-      "- 고객사 및 외주 협력사와 지속적으로 소통하며 기능 고도화 진행",
+      "- STT/TTS 기술을 활용한 학습용 Recorder 컴포넌트 개발",
+      "- 학습자의 발음평가 데이터를 ApexChart를 활용하여 시각화",
+      "- 영어 교과서 내 공통 레이아웃 컴포넌트 개발",
     ],
   },
   {
@@ -61,37 +60,35 @@ const careerProjects = [
   {
     id: 6,
     summary: {
-      projectTitle: "TmaxAI 연구본부 서버 클러스터 구축 및 백오피스 개발",
+      projectTitle: "연구본부 서버 클러스터 구축 및 백오피스 개발",
       description: "사내 AI 모델 관리를 위한 쿠버네티스 클러스터 구축",
     },
     tasks: [
-      "- 사내 AI 모델 관리를 위한 Kubernetes 클러스터 구축",
-      "- AI 모델 Triton 서버 배포 작업 수행",
-      "- Next.js 기반의 AI 모델 로드 조작 및 테스트용 백오피스 제작",
-      "- STT, TTS 및 OCR 모델 테스트 기능 개발",
+      "- 사내 AI 모델 관리를 위한 Kubernetes 클러스터 구축 및 GPU 환경세팅",
+      "- Triton을 활용한 AI 모델 배포",
+      "- Next.js을 기반으로 한 백오피스 애플리케이션 개발",
+      "- 백오피스의 모델 로드 제어 기능 및 STT/TTS 및 OCR 등의 모델 테스트 기능 개발",
     ],
   },
   {
     id: 7,
     summary: {
-      projectTitle: "Frontend 팀내 CI/CD 환경 구축",
-      description: "프론트엔드 배포 자동화를 위한 CI/CD 구축",
+      projectTitle: "Frontend CI/CD 구축",
+      description: "프론트엔드 배포 자동화를 위한 CI/CD pipline 구축",
     },
-    tasks: [
-      "- Kubernetes 클러스터 및 Jenkins를 활용한 CI/CD Pipeline 구축",
-      "- 개발 진행 모니터링 및 내부 QA 및 POC 관련 배포 작업의 편의 향상 달성",
-    ],
+    tasks: ["- Jenkins를 이용한 프론트엔드 애플리케이션 CI/CD 자동화"],
   },
   {
     id: 8,
     summary: {
-      projectTitle: "NLP 모델 테스트 클라이언트 개발",
+      projectTitle: "NLP 모델 인퍼런스 테스트페이지 개발",
       description:
-        "자사 지식 관계 플랫폼(KRP)에 활용 중인 NLP 모델 테스트 페이지 개발",
+        "자사 지식 관계 플랫폼(KRP)에 활용되는 NLP 모델 테스트 페이지 개발",
     },
     tasks: [
-      "- PDF 파싱, 요약 추출 등 파이프라인의 각 단계별 테스트 기능 구현",
-      "- FSD Architecture를 적용한 애플리케이션 개발",
+      "- 자사에서 구축한 지식 관계 플랫폼(KRP)에 활용되는 NLP 모델 인퍼런스 테스트 페이지 개발",
+      "- PDF 문서 파싱, 문장 요약 및 추출 등 파이프라인의 각 단계별로 테스트 구현",
+      "- Feature-Sliced Design 아키텍처 도입",
     ],
   },
 ];
